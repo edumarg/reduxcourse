21dcd04cb924bc3c9baeada42784e2e5288d8356
Eduardo Margulis <edumarg@gmx.com>
2021-05-23 14:56:41 +0300

    reformated store code using redux-toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,7 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
 export default function configureStore() {
-  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
+  const store = configureStore({ reducer });
   return store;
 }

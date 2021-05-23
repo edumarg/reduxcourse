dde64f90eee1999d2a69ef73ebd15874f4ceceba
Eduardo Margulis <edumarg@gmx.com>
2021-05-23 10:42:39 +0300

    created custom store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import { sortedIndex } from "lodash";
-import store from "./store";
+import store from "./customStrore";
 import * as actions from "./actions";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Store changed", store.getState());
+store.subscribe(() => {
+  console.log("Subscribe changed!!");
 });
 
-store.dispatch(actions.bugAdded("Bug 1!!"));
-
-store.dispatch(actions.bugResolved(1));
-
-unsubscribe();
-
-store.dispatch(actions.bugRemoved(1));
-
-console.log("--store--", store.getState());
+store.dispatch(actions.bugAdded("bug 1!!!"));
+console.log(store.getState());

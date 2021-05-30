36d2bcae2ff3a4fdbac348b9984f388313a5295a
Eduardo Margulis <edumarg@gmx.com>
2021-05-30 14:08:20 +0300

    reformated addBug test using AAA patern

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,19 +1,42 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 
-import { addBug, bugAdded } from "./../bugs";
+import { addBug } from "./../bugs";
 import configureStore from "./../configureStore";
 
 describe("bugsSlice", () => {
-  it("Should handle the addBug action", async () => {
+  let fakeAxios;
+  let store;
+
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios);
+    store = configureStore();
+  });
+
+  const bugsSlice = () => store.getState().entities.bugs;
+
+  it("Should add bug to store if it's saved to the server", async () => {
+    // Arange
     const bug = { description: "a" };
     const savedBug = { ...bug, id: 1 };
-    const fakeAxios = new MockAdapter(axios);
     fakeAxios.onPost("/bugs").reply(200, savedBug);
-    // dispatch(addbug) => store
-    const store = configureStore();
 
+    // Act
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+
+    // Assert
+    expect(bugsSlice().list).toContainEqual(savedBug);
+  });
+
+  it("Should not add bug to store if it's not saved to the server", async () => {
+    // Arange
+    const bug = { description: "a" };
+    fakeAxios.onPost("/bugs").reply(500);
+
+    // Act
+    await store.dispatch(addBug(bug));
+
+    // Assert
+    expect(bugsSlice().list).toHaveLength(0);
   });
 });
